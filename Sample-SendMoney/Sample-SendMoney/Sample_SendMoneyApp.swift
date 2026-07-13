59276c9967eece2470975ad2d70c7f2fa7d73b5b
Keith Samson <keithmarksamson@gmail.com>
2026-07-13 10:26:43 +0800

    Initial commit.

---
+++ Sample-SendMoney/Sample-SendMoney/Sample_SendMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_SendMoneyApp.swift
+//  Sample-SendMoney
+//
+//  Created by Keith Samson on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Sample_SendMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
